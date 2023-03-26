3883674d80c0d37150ba40f281161c578d35b0fe
Ignatov Dan <ignatovdan@gmail.com>
2023-03-26 17:35:39 +0300

    Add createPlacesList

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,7 @@ const BEM_FORM__INPUT_VALIDATION_MESSAGE = 'form__input-validation-message';
 
 function createPopup(popupEl) {
   const BEM_POPUP_OPENED = 'popup_opened';
+
   const result = {};
   result.popupEl = popupEl;
   result.popupCloseEl = result.popupEl.querySelector('.popup__close');
@@ -10,26 +11,20 @@ function createPopup(popupEl) {
   result.hide = () => {
     result.popupEl.classList.remove(BEM_POPUP_OPENED);
   };
+
   result.show = () => {
     result.popupEl.classList.add(BEM_POPUP_OPENED);
   };
+
   result.popupCloseEl.addEventListener('click', (evt) => {
     // clear form/elements values ???
     // result.closingEvent?.();
     result.hide(popupEl);
   });
 
-  return result;
-}
-
-function initializePopups() {
-  const BEM_POPUP = 'popup';
-
   function removePopupPageIsLoadingState() {
-      // use querySelectorAll because long list of event handlers works slow
-      for (let popup of document.querySelectorAll(`.${BEM_POPUP}`)) {
-      popup.classList.remove('popup_page-is-loading');
-    }
+    // Initially hide popup to avoid flicks on page loading when CSS files are not yet loaded
+    result.popupEl.classList.remove('popup_page-is-loading');
     document.removeEventListener('DOMContentLoaded', removePopupPageIsLoadingState);
   }
 
@@ -39,6 +34,8 @@ function initializePopups() {
   } else {
     removePopupPageIsLoadingState();
   }
+
+  return result;
 }
 
 function createProfileSection(sectionEl) {
@@ -59,7 +56,7 @@ function createProfileSection(sectionEl) {
     result.onAddPlace?.();
   });
 
-  result.updateProfileElements = ({ name, details }) => {
+  result.setProfileValues = ({ name, details }) => {
     result.nameEl.textContent = name;
     result.detailsEl.textContent = details;
   };
@@ -74,6 +71,9 @@ function createEditProfilePopup(popupEl) {
   result.popup = createPopup(popupEl);
 
   result.formEl = result.popupEl.querySelector('.profile-form');
+  result.nameInput = result.formEl.querySelector('.profile-form__input_name');
+  result.detailsInput = result.formEl.querySelector('.profile-form__input_details');
+
   result.formEl.addEventListener('submit', (evt) => {
     evt.preventDefault();
     result.onSubmit?.({
@@ -82,9 +82,6 @@ function createEditProfilePopup(popupEl) {
     });
     result.popup.hide();
   });
-
-  result.nameInput = result.formEl.querySelector('.profile-form__input_name');
-  result.detailsInput = result.formEl.querySelector('.profile-form__input_details');
 
   result.show = ({ name, details }) => {
     result.nameInput.value = name;
@@ -101,6 +98,9 @@ function createAddPlacePopup(popupEl) {
   result.popup = createPopup(popupEl);
 
   result.formEl = result.popupEl.querySelector('.add-place-form');
+  result.nameInput = result.formEl.querySelector('.add-place-form__input_name');
+  result.linkInput = result.formEl.querySelector('.add-place-form__input_link');
+
   result.formEl.addEventListener('submit', (evt) => {
     evt.preventDefault();
     result.onSubmit?.({
@@ -110,22 +110,19 @@ function createAddPlacePopup(popupEl) {
     result.popup.hide();
   });
 
-  result.nameInput = result.formEl.querySelector('.add-place-form__input_name');
-  result.linkInput = result.formEl.querySelector('.add-place-form__input_link');
-
   result.show = () => {
     result.formEl.querySelectorAll('.form__input').forEach((inputEl) => {
       inputEl.value = '';
       inputEl.classList.add(BEM_FORM__INPUT_INITIAL_STATE);
     });
-  
+
     result.formEl.querySelectorAll(`.${BEM_FORM__INPUT_VALIDATION_MESSAGE}`).forEach((messageEl) => {
       messageEl.content = '';
     });
-  
+
     result.popup.show();
   }
-  
+
   return result;
 }
 
@@ -154,45 +151,95 @@ function createPlace(placeEl) {
   result.likeEl = result.placeEl.querySelector('.place__like');
   result.deleteEl = result.placeEl.querySelector('.place__delete');
 
-  result.imageEl.addEventListener('click', () => result.onImageClick?.());
-  result.likeEl.addEventListener('click', () => result.onLikePlace?.());
-  result.deleteEl.addEventListener('click', () => result.onDeletePlace?.());  
+  result.imageEl.addEventListener('click', () => result.onShowDetails?.({ place: result, values: result.getPlaceValues() }));
+  result.likeEl.addEventListener('click', () => result.onLikePlace?.({ place: result, values: result.getPlaceValues() }));
+  result.deleteEl.addEventListener('click', () => result.onDeletePlace?.({ place: result }));
 
-  result.bind = ({ name, link }) => {
+  result.setPlaceValues = ({ name, link, like }) => {
     result.imageEl.src = link;
     result.captionEl.textContent = name;
+    result.likeEl.classList.toggle('place__like_active', !!like);
   };
-  result.toggleLike = () => {
-    result.likeEl.classList.toggle('place__like_active');
+
+  result.getPlaceValues = () => {
+    return {
+      name: result.captionEl.textContent,
+      link: result.imageEl.src,
+      like: result.likeEl.classList.contains('place__like_active')
+    };
   };
 
   return result;
 }
 
+function createPlacesList(placesListEl, { placesListItemTemplate, placeTemplate }) {
+  const result = {};
+  result.placesListEl = placesListEl;
+
+  result.createPlacesListItemFromTemplateEl = ({ name, link }) => {
+    const result = {};
+    const placesListItemEl = placesListItemTemplate.cloneNode(true);
+    result.placesListItemEl = placesListItemEl;
+
+    const placeEl = placeTemplate.cloneNode(true);
+    placesListItemEl.append(placeEl);
+
+    const place = createPlace(placeEl);
+    place.onLikePlace = (evt) => evt.place.setPlaceValues({ ...evt.values, like: !evt.values.like });
+    place.onDeletePlace = () => placesListItemEl.remove();
+    place.onShowDetails = (evt) => result.onShowPlaceDetails?.({ name: evt.values.name, link: evt.values.link });
+    place.setPlaceValues({ name, link });
+
+    return result;
+  };
+
+  result.createItemFromTemplate = ({ name, link }) => {
+    const placesListItem = result.createPlacesListItemFromTemplateEl({ name, link });
+    placesListItem.onShowPlaceDetails = ({ name, link }) => result.onShowPlaceDetails?.({ name, link });
+    return placesListItem;
+  };
+
+  result.showPlaces = (places) => {
+    places.forEach(({ name, link }) => {
+      const placesListItem = result.createItemFromTemplate({ name, link });
+      result.placesListEl.append(placesListItem.placesListItemEl);
+    });
+  };
+
+  result.addPlace = ({ name, link }) => {
+    const placesListItem = result.createItemFromTemplate({ name, link });
+    result.placesListEl.prepend(placesListItem.placesListItemEl);
+  };
+
+  return result;
+}
+
+const placesListItemTemplate = document.querySelector('#places-list-item-template').content.querySelector('.places-list__item');
+const placeTemplate = document.querySelector('#place-template').content.querySelector('.place');
+
 const editProfilePopup = createEditProfilePopup(document.querySelector('.popup_type_edit-profile'));
 const profileSection = createProfileSection(document.querySelector('.profile'));
 const addPlacePopup = createAddPlacePopup(document.querySelector('.popup_type_add-place'));
 const viewPlacePopup = createViewPlacePopup(document.querySelector('.popup_type_view-place'));
+const placesList = createPlacesList(document.querySelector('.places-list'), { placesListItemTemplate, placeTemplate });
 
 profileSection.onEditProfile = ({ name, details }) => {
   editProfilePopup.show({ name, details });
 }
+
 profileSection.onAddPlace = () => {
   addPlacePopup.show();
 }
 
 editProfilePopup.onSubmit = ({ name, details }) => {
-  profileSection.updateProfileElements({ name, details });
+  profileSection.setProfileValues({ name, details });
 };
 
 addPlacePopup.onSubmit = ({ name, link }) => {
-  const placesListItem = createPlacesListItemFromTemplateEl({ name, link });
-  placesListEl.prepend(placesListItem.placesListItemEl);
+  placesList.addPlace({ name, link });
 }
 
-const placesListItemTemplate = document.querySelector('#places-list-item-template').content.querySelector('.places-list__item');
-const placeTemplate = document.querySelector('#place-template').content.querySelector('.place');
-const placesListEl = document.querySelector('.places-list');
+placesList.onShowPlaceDetails = ({ name, link }) => viewPlacePopup.show({ caption: name, link: link });
 
 const initialCards = [
   {
@@ -221,37 +268,8 @@ const initialCards = [
   }
 ];
 
-function createPlacesListItemFromTemplateEl(card) {
-  const result = {};
-  const placesListItemEl = placesListItemTemplate.cloneNode(true);
-  result.placesListItemEl = placesListItemEl;
-
-  const placeEl = placeTemplate.cloneNode(true);
-  placesListItemEl.append(placeEl);
-
-  const place = createPlace(placeEl);
-  place.onLikePlace = () => place.toggleLike();
-  place.onDeletePlace = () => placesListItemEl.remove();
-  place.onImageClick = () => {
-    viewPlacePopup.show({ // TODO: remove reference to global variable
-      caption: place.captionEl.textContent,
-      link: place.imageEl.src,
-    });
-  }
-  place.bind(card);
-
-  return result;
-}
-
-function renderPlacesList(cards) {
-  cards.forEach(card => {
-    const placesListItem = createPlacesListItemFromTemplateEl(card);
-    placesListEl.append(placesListItem.placesListItemEl);
-  });
-}
-
 document.querySelectorAll('.form__input-group').forEach((inputGroupEl) => {
-  const inputInvalidModifier = 'form__input_invalid';
+  const BEM_FORM__INPUT_INVALID_MODIFIER = 'form__input_invalid';
 
   const validationMessageEl = inputGroupEl.querySelector(`.${BEM_FORM__INPUT_VALIDATION_MESSAGE}`);
   const inputEl = inputGroupEl.querySelector('.form__input');
@@ -261,15 +279,14 @@ document.querySelectorAll('.form__input-group').forEach((inputGroupEl) => {
     inputEl.classList.remove(BEM_FORM__INPUT_INITIAL_STATE);
     if (evt.target.validity.valid) {
       validationMessageEl.textContent = ''; // empty content sets 0px height and element is not visible
-      inputEl.classList.remove(inputInvalidModifier);
+      inputEl.classList.remove(BEM_FORM__INPUT_INVALID_MODIFIER);
     }
     else {
-      // there can be smooth show/hide similar to popup show/hide
+      // can be better: implement smooth show/hide similar to popup show/hide
       validationMessageEl.textContent = evt.target.validationMessage;
-      inputEl.classList.add(inputInvalidModifier);
+      inputEl.classList.add(BEM_FORM__INPUT_INVALID_MODIFIER);
     }
   });
 });
 
-renderPlacesList(initialCards);
-initializePopups();
+placesList.showPlaces(initialCards);
