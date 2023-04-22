631021b6ab1ae8bcfe4aeabdc86d17aa090918a8
Ignatov Dan <ignatovdan@gmail.com>
2023-04-23 00:03:39 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -109,7 +109,11 @@ const addCards = (name, link) => {
   cardTemplate.querySelector('.card__img').addEventListener('click', () => {
 
     photoPopup.querySelector('.photo-popup__image').src = link;
+- querySelector возвращает один и тот же элемент на каждом вызове
+Для этой ситуации нужно объявить const переменные, выполнить поиск один раз и сохранить значения начале файла.
+Самостоятельно проверьте аналогичный поиск элементов в остальном коде и добавьте const переменные.
     photoPopup.querySelector('.photo-popup__caption').textContent = name;
+- querySelector возвращает один и тот же элемент на каждом вызове
 
     togglePopupClass(photoPopup);
   });
